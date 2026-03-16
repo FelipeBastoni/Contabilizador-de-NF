3c9d249fc56e3e29717f9948386152da72b7b1bb
Felipe Bertão Bastoni <felipebertaobastoni@gmail.com>
2026-03-16 17:40:08 -0300

    Update nfxml.py

--- Contabilizador-de-NF/Ecommerce/nfxml.py
+++ Contabilizador-de-NF/Ecommerce/nfxml.py
@@ -7,7 +7,57 @@ import os
 def init():
 
 
+    def computxml():
+
+        db = pd.read_excel("C:/Users/proje/OneDrive/Documentos/dbxlsx.xlsx", sheet_name="Faturamento")
+
+        
+        db[["Valor", "Unidades", "Num. Vendas"]] = 0
+
+        db.loc[0,"Total"] = 0
+
+        with pd.ExcelWriter("C:/Users/proje/OneDrive/Documentos/dbxlsx.xlsx", engine="openpyxl", mode="a", if_sheet_exists="replace") as writer:
+            db.to_excel(writer, sheet_name="Faturamento", index=False)
+
+
+
+    def proceed():
+
+
+        db = pd.read_excel("C:/Users/proje/OneDrive/Documentos/dbxlsx.xlsx", sheet_name="Faturamento")
+                
+
+        valordb = db.loc[db["Produto"] == "$"+cProd, "Valor"].values[0]
+        nv_valor = valordb + vProd
+
+        db.loc[db['Produto'] == "$"+cProd, 'Valor'] = nv_valor
+
+
+
+        quantdb = db.loc[db["Produto"] == "$"+cProd, "Unidades"].values[0]
+        nv_quant = quantdb + qCom
+
+        db.loc[db['Produto'] == "$"+cProd, 'Unidades'] = nv_quant
+
+
+
+        nmvendasdb = db.loc[db["Produto"] == "$"+cProd, "Num. Vendas"].values[0]
+        nv_nmvd = nmvendasdb + 1
+
+        db.loc[db['Produto'] == "$"+cProd, 'Num. Vendas'] = nv_nmvd
+
+
+        with pd.ExcelWriter("C:/Users/proje/OneDrive/Documentos/dbxlsx.xlsx", engine="openpyxl", mode="a", if_sheet_exists="replace") as writer:
+            db.to_excel(writer, sheet_name="Faturamento", index=False)
+
+
+
+
+
+    computxml()
+
     pasta = "Xml"
+
 
     for xml in os.listdir(pasta):
 
@@ -20,59 +70,21 @@ def init():
 
         print(xml)
 
-        def computxml():
-
-            db = pd.read_excel("C:/Users/proje/OneDrive/Documentos/dbxlsx.xlsx", sheet_name="Faturamento")
-
-            
-            db[["Valor", "Unidades", "Num. Vendas"]] = 0
-
-            db.loc[0,"Total"] = 0
-
-            with pd.ExcelWriter("C:/Users/proje/OneDrive/Documentos/dbxlsx.xlsx", engine="openpyxl", mode="a", if_sheet_exists="replace") as writer:
-                db.to_excel(writer, sheet_name="Faturamento", index=False)
 
 
+        for det in root.findall(".//nfe:det", ns):
 
-            def proceed():
+            prod = det.find("nfe:prod", ns)
 
-                
-                valordb = db.loc[db["Produto"] == "$"+cProd, "Valor"].values[0]
-                nv_valor = valordb + vProd
+            cProd = prod.find("nfe:cProd", ns).text
+            vProd = float(prod.find("nfe:vProd", ns).text)
+            qCom = float(prod.find("nfe:qCom", ns).text)
 
-                db.loc[db['Produto'] == "$"+cProd, 'Valor'] = nv_valor
-
-
-
-                quantdb = db.loc[db["Produto"] == "$"+cProd, "Unidades"].values[0]
-                nv_quant = quantdb + qCom
-
-                db.loc[db['Produto'] == "$"+cProd, 'Unidades'] = nv_quant
+            print(cProd, vProd, qCom)
+            print("\n")
 
 
-
-                nmvendasdb = db.loc[db["Produto"] == "$"+cProd, "Num. Vendas"].values[0]
-                nv_nmvd = nmvendasdb + 1
-
-                db.loc[db['Produto'] == "$"+cProd, 'Num. Vendas'] = nv_nmvd
+            proceed()
 
 
-                with pd.ExcelWriter("C:/Users/proje/OneDrive/Documentos/dbxlsx.xlsx", engine="openpyxl", mode="a", if_sheet_exists="replace") as writer:
-                    db.to_excel(writer, sheet_name="Faturamento", index=False)
-
-
-
-
-            for det in root.findall(".//nfe:det", ns):
-
-                prod = det.find("nfe:prod", ns)
-
-                cProd = prod.find("nfe:cProd", ns).text
-                vProd = float(prod.find("nfe:vProd", ns).text)
-                qCom = float(prod.find("nfe:qCom", ns).text)
-
-                print(cProd, vProd, qCom)
-                print("\n")
-
-                proceed()
-
+    return True
